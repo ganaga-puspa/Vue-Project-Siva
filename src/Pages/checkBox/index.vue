a6195b588148a0d86833151fbd3577dc1a17eeb3
ganaga-puspa <ganaga-puspa>
2023-05-03 03:26:41 +0530

    project-init

---
+++ src/Pages/checkBox/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <input type="checkbox" id="checkbox" v-model="checked" />
+    <label for="checkbox">checkBox</label>
+  </div>
+</template>
+<script>
+export default {
+  name: "CheckBox",
+};
+</script>
+<style></style>
